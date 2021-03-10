6770c1c0d550fe3f4f32bbbd119dca3aaea6d3b1
NatalliaKri <55781208+NatalliaKri@users.noreply.github.com>
2021-03-10 21:27:17 +0300

    init: implement function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
+
 module.exports = function reverse (n) {
-  
+	
+	let newArr = [];
+	let d = Math.abs(n);
+	
+	d = String(d);
+	let t = d.split('');
+	
+	
+	for(let i = t.length - 1; i >= 0; i--){
+		if(t[i] === '0'){
+			t.splice(i, 1);
+		};
+		
+		if(t[i] !== '0'){
+			break;
+		}
+	};
+	
+	
+	for( let i = t.length - 1; i >= 0; i--){
+		
+		let digit = t.slice(i, i + 1);
+		newArr.push(digit);
+	}
+	
+  return  newArr.join(''); 
 }
+
